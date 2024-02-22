96534b3c5128f86a1bc6a0614d43b897bc33a59f
vsafonova <viktoriyablak@gmail.com>
2024-02-22 15:56:30 +0100

    Working with routing on Navbar

---
+++ src/components/Card.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+
+export default function CitiesSection() {
+
+const cities = [
+  {
+    srcImage: 'images/img-1.png',
+    cityName: 'Stockholm'
+  },
+
+  {
+    srcImage: 'images/img-2.png',
+    cityName: 'Visby'
+  },
+
+  {
+    srcImage: 'images/img-3.png',
+    cityName: 'Järvsö'
+  },
+]
+
+
+  return (
+    <section className="services_section layout_padding" id="cities">
+      <div className="container">
+        <h1 className="services_taital" id="cities">
+          Cities
+        </h1>
+        <p className="services_text">
+        Embark on a journey of discovery as you explore the hidden gems of Sweden, where every corner reveals a new adventure waiting to be uncovered</p>
+        <div className="services_section_2">
+          <div className="row">
+            <div className="col-md-4">
+              <div>
+                <img src="images/img-1.png" className="services_img" />
+              </div>
+              <div className="btn_main">
+                <Link to="/CityPage">Stockholm</Link>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div>
+                <img src="images/img-2.png" className="services_img" />
+              </div>
+              <div className="btn_main">
+                <Link to="/CityPage">Visby</Link>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div>
+                <img src="images/img-3.png" className="services_img" />
+              </div>
+              <div className="btn_main">
+                <Link to="/CityPage">Järvsö</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/CitiesSection.jsx
+++ src/components/CitiesSection.jsx
@@ -1,6 +1,27 @@
 import { Link } from "react-router-dom";
 
 export default function CitiesSection() {
+
+const cities = [
+  {
+    srcImage: 'images/img-1.png',
+    cityName: 'Stockholm'
+  },
+
+  {
+    srcImage: 'images/img-2.png',
+    cityName: 'Visby'
+  },
+
+  {
+    srcImage: 'images/img-3.png',
+    cityName: 'Järvsö'
+  },
+]
+
+
+
+
   return (
     <section className="services_section layout_padding" id="cities">
       <div className="container">
@@ -8,9 +29,7 @@ export default function CitiesSection() {
           Cities
         </h1>
         <p className="services_text">
-          There are many variations of passages of Lorem Ipsum available, but
-          the majority have suffered alteration
-        </p>
+        Embark on a journey of discovery as you explore the hidden gems of Sweden, where every corner reveals a new adventure waiting to be uncovered</p>
         <div className="services_section_2">
           <div className="row">
             <div className="col-md-4">
