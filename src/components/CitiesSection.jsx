d95178d4db8ebe5519c0d4f203b46b652afa5e82
vsafonova <viktoriyablak@gmail.com>
2024-02-22 23:23:09 +0100

    Refactoring Card and CardSection

--- src/components/CitiesSection.jsx
+++ src/components/CitiesSection.jsx
@@ -1,26 +1,26 @@
 import { Link } from "react-router-dom";
+import Card from "./Card";
 
 export default function CitiesSection() {
+  const cities = [
+    {
+      image: "img-1.png",
+      cityName: "Stockholm",
+      link: "/CityPage",
+    },
 
-const cities = [
-  {
-    srcImage: 'images/img-1.png',
-    cityName: 'Stockholm'
-  },
+    {
+      image: "img-2.png",
+      cityName: "Visby",
+      link: "/CityPage",
+    },
 
-  {
-    srcImage: 'images/img-2.png',
-    cityName: 'Visby'
-  },
-
-  {
-    srcImage: 'images/img-3.png',
-    cityName: 'Järvsö'
-  },
-]
-
-
-
+    {
+      image: "img-3.png",
+      cityName: "Järvsö",
+      link: "/CityPage",
+    },
+  ];
 
   return (
     <section className="services_section layout_padding" id="cities">
@@ -29,33 +29,17 @@ const cities = [
           Cities
         </h1>
         <p className="services_text">
-        Embark on a journey of discovery as you explore the hidden gems of Sweden, where every corner reveals a new adventure waiting to be uncovered</p>
+          Embark on a journey of discovery as you explore the hidden gems of
+          Sweden, where every corner reveals a new adventure waiting to be
+          uncovered
+        </p>
         <div className="services_section_2">
           <div className="row">
-            <div className="col-md-4">
-              <div>
-                <img src="images/img-1.png" className="services_img" />
+            {cities.map(({image, cityName, link}) => (
+              <div className="col-md-4" key={cityName}>
+                <Card image={image} cityName={cityName} link={link} />
               </div>
-              <div className="btn_main">
-                <Link to="/CityPage">Stockholm</Link>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div>
-                <img src="images/img-2.png" className="services_img" />
-              </div>
-              <div className="btn_main">
-                <Link to="/CityPage">Visby</Link>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div>
-                <img src="images/img-3.png" className="services_img" />
-              </div>
-              <div className="btn_main">
-                <Link to="/CityPage">Järvsö</Link>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
