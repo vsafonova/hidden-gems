c20ad606de21d5598d8cfd8dae1914070e83c5ef
millalinden <milla.linden@hyperisland.se>
2024-02-26 13:17:29 +0100

    footer fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 export default function Footer() {
   return (
-    <footer id="footer" className="footer_section layout_padding"></footer>
+    <footer id="footer" className="footer_section layout_padding">
+      <p>Hidden Gems AB 2024</p>
+    </footer>
   );
 }
