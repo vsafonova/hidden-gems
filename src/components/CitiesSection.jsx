5f646dd8b136130b93e3ffd6511e4377274e46f1
vsafonova <viktoriyablak@gmail.com>
2024-02-26 16:48:32 +0100

    Format text with prettier

--- src/components/CitiesSection.jsx
+++ src/components/CitiesSection.jsx
@@ -13,14 +13,16 @@ export default function CitiesSection() {
       image: "visby.webp",
       cityName: "Visby",
       link: "/Visby",
-      altText: "Visby city from above. White houses with orange roofs overlooking the ocean.",
+      altText:
+        "Visby city from above. White houses with orange roofs overlooking the ocean.",
     },
 
     {
       image: "järvsö.webp",
       cityName: "Järvsö",
       link: "/Järvsö",
-      altText: "Three people biking down a hill. Scenery with a lake and forest.",
+      altText:
+        "Three people biking down a hill. Scenery with a lake and forest.",
     },
   ];
 
@@ -29,13 +31,27 @@ export default function CitiesSection() {
       <div className="container">
         <h2 className="cities_taital">Cities</h2>
         <p className="cities_text">
-        Embark on a journey of discovery as you explore the hidden gems of Sweden, where every corner reveals a new adventure waiting to be uncovered. Our curated selection of unique and lesser-known destinations invites you to delve deeper into the heart and soul of Sweden. We believe in open exploration without gatekeeping, ensuring that everyone has the opportunity to uncover the extraordinary beauty and richness that Sweden has to offer. So, whether you're a seasoned traveler or a curious explorer, join us as we unlock the hidden treasures of this remarkable land and create unforgettable memories along the way
+          Embark on a journey of discovery as you explore the hidden gems of
+          Sweden, where every corner reveals a new adventure waiting to be
+          uncovered. Our curated selection of unique and lesser-known
+          destinations invites you to delve deeper into the heart and soul of
+          Sweden. We believe in open exploration without gatekeeping, ensuring
+          that everyone has the opportunity to uncover the extraordinary beauty
+          and richness that Sweden has to offer. So, whether you're a seasoned
+          traveler or a curious explorer, join us as we unlock the hidden
+          treasures of this remarkable land and create unforgettable memories
+          along the way
         </p>
         <div className="cities_section_2">
           <div className="row">
             {cities.map(({ image, cityName, link, altText }) => (
               <div className="col-md-4" key={cityName}>
-                <Card image={image} cityName={cityName} link={link} altText={altText} />
+                <Card
+                  image={image}
+                  cityName={cityName}
+                  link={link}
+                  altText={altText}
+                />
               </div>
             ))}
           </div>
--- src/pages/Järvsö.jsx
+++ src/pages/Järvsö.jsx
@@ -2,53 +2,77 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export default function Järvsö() {
-  return (    <>
-    <header id="header-section" className="header_section">
-      <Navbar />
-    </header>
-    <section className="about_section layout_padding">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="about_taital_main">
-              <h1 className="about_taital">Järvsö</h1>
-              <p className="about_text">
-              Explore the charming Järvsö, a quaint city bursting with exciting adventures! Regardless of the season, there's always something thrilling to experience in this hidden gem.
-              </p>
-              <p className="about_text">
-                {" "}
-                In the winter months, Järvsö transforms into a skier's paradise with its Järvsöbacken boasting 23 diverse hills catering to all skill levels, from seasoned experts to little ones taking their first turns on the slopes.
-              </p>
-              <p className="about_text">
-                {" "}
-                Come summer, adrenaline junkies can trade their skis for mountain bikes and conquer the rugged terrain with daring descents and exhilarating trails. For those seeking a different kind of adventure, immerse yourself in the heart of Swedish nature at Vildriket, a unique zoo where you stroll through the forest, seamlessly blending into the natural habitat alongside majestic creatures like lynxes and snow foxes.{" "}
-              </p>{" "}
-              <p className="about_text">
-              When it's time to eat, we recommend Järvsö Creperie, a small and charming spot that offers both savoury and sweet crepes. How about a crêpe with local cloudberries and a generous scoop of vanilla – a true taste of the region's flavours.
-              </p>
-              <p className="about_text">
-                {" "}
-                And for connoisseurs of architecture and serene sanctuaries, Järvsö proudly boasts one of the largest and most spectacular churches in the northern region of Sweden built in 1832–1838. The church is nestled on a hill overlooking the water with a spectacular view.
-              </p>
-              <p className="about_text">
-                {" "}
-                Järvsö invites you to discover its hidden treasures and unforgettable experiences, beckoning you to embark on a journey like no other. Come explore the beautiful nature of and create memories that will last a lifetime in this hidden gem of Sweden.
-              </p>
+  return (
+    <>
+      <header id="header-section" className="header_section">
+        <Navbar />
+      </header>
+      <section className="about_section layout_padding">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="about_taital_main">
+                <h1 className="about_taital">Järvsö</h1>
+                <p className="about_text">
+                  Explore the charming Järvsö, a quaint city bursting with
+                  exciting adventures! Regardless of the season, there's always
+                  something thrilling to experience in this hidden gem.
+                </p>
+                <p className="about_text">
+                  {" "}
+                  In the winter months, Järvsö transforms into a skier's
+                  paradise with its Järvsöbacken boasting 23 diverse hills
+                  catering to all skill levels, from seasoned experts to little
+                  ones taking their first turns on the slopes.
+                </p>
+                <p className="about_text">
+                  {" "}
+                  Come summer, adrenaline junkies can trade their skis for
+                  mountain bikes and conquer the rugged terrain with daring
+                  descents and exhilarating trails. For those seeking a
+                  different kind of adventure, immerse yourself in the heart of
+                  Swedish nature at Vildriket, a unique zoo where you stroll
+                  through the forest, seamlessly blending into the natural
+                  habitat alongside majestic creatures like lynxes and snow
+                  foxes.{" "}
+                </p>{" "}
+                <p className="about_text">
+                  When it's time to eat, we recommend Järvsö Creperie, a small
+                  and charming spot that offers both savoury and sweet crepes.
+                  How about a crêpe with local cloudberries and a generous scoop
+                  of vanilla – a true taste of the region's flavours.
+                </p>
+                <p className="about_text">
+                  {" "}
+                  And for connoisseurs of architecture and serene sanctuaries,
+                  Järvsö proudly boasts one of the largest and most spectacular
+                  churches in the northern region of Sweden built in 1832–1838.
+                  The church is nestled on a hill overlooking the water with a
+                  spectacular view.
+                </p>
+                <p className="about_text">
+                  {" "}
+                  Järvsö invites you to discover its hidden treasures and
+                  unforgettable experiences, beckoning you to embark on a
+                  journey like no other. Come explore the beautiful nature of
+                  and create memories that will last a lifetime in this hidden
+                  gem of Sweden.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="col-md-6 padding_right_0">
-            <div>
-              <img
-                src=".\images\järvsö-сity.webp"
-                className="about_img"
-                alt="About Hidden Gems"
-              />
+            <div className="col-md-6 padding_right_0">
+              <div>
+                <img
+                  src=".\images\järvsö-сity.webp"
+                  className="about_img"
+                  alt="About Hidden Gems"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-    <Footer />
-  </>
-);
+      </section>
+      <Footer />
+    </>
+  );
 }
